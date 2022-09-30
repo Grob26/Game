1ff01e9c5d2adb7cee7a57de40ef49c779444f37
Drobik11 <97844085+Grobik2@users.noreply.github.com>
2022-09-30 23:44:33 +0300

    1

--- compass.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function () {
-    scrollRotate();
-};
-
-function scrollRotate() {
-    let image = document.getElementById("compass");
-    image.style.transform = "rotate(" + window.pageYOffset/100 + "deg)";
-}
