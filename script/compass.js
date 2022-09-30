696cad85bd2c0ad2b832b6c7c4e0ed5013619cc6
xarlande <0966312126a@gmail.com>
2022-09-30 21:40:51 +0300

    добавив js для компаса

---
+++ script/compass.js
@@ -0,0 +1,8 @@
+window.onscroll = function () {
+    scrollRotate();
+};
+
+function scrollRotate() {
+    let image = document.getElementById("compass");
+    image.style.transform = "rotate(" + window.pageYOffset/100 + "deg)";
+}
